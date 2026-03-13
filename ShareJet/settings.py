ec57ce8c8fcd5af0f1c584d5d85f80cca16dbf8c
Kartik-devp <kartiktelote0804@gmail.com>
2026-03-13 18:45:13 +0530

    Allow .onrender.com domains by default

--- ShareJet/settings.py
+++ ShareJet/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-h268+9=x^kbt1ki)8=dv0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,.onrender.com').split(',')
 
 
 # Application definition
