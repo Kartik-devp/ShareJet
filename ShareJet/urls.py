5d1e086193a35d7c8bc0f5b165717682520d9037
Kartik-devp <kartiktelote0804@gmail.com>
2026-03-13 18:59:03 +0530

    Add whitenoise for static file serving on Render

--- ShareJet/urls.py
+++ ShareJet/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -7,5 +9,5 @@ urlpatterns = [
     path('api/auth/', include('accounts.urls')),
     path('api/transfers/', include('transfers.urls')),
     path('api/files/', include('files.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
